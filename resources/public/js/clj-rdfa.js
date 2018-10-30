d9b307b32210a42d14e6d9fbd0f117a3cbe69787
Jochen Rau <jrau@infinitecloud.com>
2018-10-30 17:18:04 -0400

    Comple JS artifacs

--- resources/public/js/clj-rdfa.js
+++ resources/public/js/clj-rdfa.js
@@ -1,7 +1,11 @@
-var CLOSURE_UNCOMPILED_DEFINES = null;
+var CLOSURE_UNCOMPILED_DEFINES = {};
+var CLOSURE_NO_DEPS = true;
 if(typeof goog == "undefined") document.write('<script src="public/js/target/goog/base.js"></script>');
+document.write('<script src="public/js/target/goog/deps.js"></script>');
 document.write('<script src="public/js/target/cljs_deps.js"></script>');
 document.write('<script>if (typeof goog == "undefined") console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?");</script>');
-
-document.write("<script>if (typeof goog != \"undefined\") { goog.require(\"figwheel.connect.dev\"); }</script>");
+document.write('<script>goog.require("figwheel.connect");</script>');
+document.write('<script>goog.require("process.env");</script>');
 document.write('<script>goog.require("rdfa.api");</script>');
+
+document.write("<script>figwheel.connect.start();</script>");

4c792901143a15f698305868f535ef4c1b7ebdf1
Jochen Rau <jrau@infinitecloud.com>
2018-11-17 12:50:53 -0500

    Make figwheel work with dev config file

--- dev-resources/figwheel.clj
+++ dev-resources/figwheel.clj
@@ -1,14 +1,2 @@
 (require '[figwheel.main.api :as fig])
-(fig/start {:id      "dev"
-            :options {:main                 rdfa.api
-                      :asset-path           "public/js/target"
-                      :output-to            "resources/public/js/clj-rdfa.js"
-                      :output-dir           "resources/public/js/target"
-                      :optimizations        :none
-                      :source-map-timestamp true
-                      :verbose              false
-                      :pretty-print         true}
-            :config  {:open-url       false
-                      :rebel-readline false
-                      :auto-test      true
-                      :watch-dirs     ["src/cljc" "src/cljs" "test/cljc"]}})
+(fig/start "dev")
